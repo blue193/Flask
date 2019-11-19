9e805ef356a7cea0b53ba71ff1380d9cb8470c08
blueeye113 <psk.baby0306@gmail.com>
2019-11-19 13:22:22 +0300

    flask appbuilder

---
+++ env/Scripts/fabmanager-script.py
@@ -0,0 +1,12 @@
+#!d:\practise\flask\myflask\env\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Flask-AppBuilder==2.2.0','console_scripts','fabmanager'
+__requires__ = 'Flask-AppBuilder==2.2.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('Flask-AppBuilder==2.2.0', 'console_scripts', 'fabmanager')()
+    )
